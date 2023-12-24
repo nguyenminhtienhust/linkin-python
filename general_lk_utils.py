453fd38945c967ab5787d7104ec805428fbd070f
MinhTien <nguyenminhtiendeveloper@gmail.com>
2023-12-25 00:01:18 +0700

    start

--- general_lk_utils.py
+++ general_lk_utils.py
@@ -1,4 +1,10 @@
 import time
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -9,6 +15,77 @@ from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
 SELECT_CONTRACT_BUTTON_SELECTOR = "#main > div > div > div:nth-child(3) > form > div > ul > li:nth-child(1) > div > div.contract-list__item-buttons > button"
 
+def get_job_detail(driver,job_id):
+    root_window = driver.window_handles[0]
+    driver.execute_script("window.open('');")
+    job_detail_window = driver.window_handles[1]
+    driver.switch_to.window(job_detail_window)
+
+    time.sleep(2)
+    job_detail_url = 'https://www.linkedin.com/jobs/view/' + job_id
+    print(job_detail_url)
+    time.sleep(3)
+    driver.get(job_detail_url)
+    time.sleep(3)
+    
+    infos_list = driver.find_element(By.CLASS_NAME,"job-details-jobs-unified-top-card__primary-description-without-tagline")
+    company_url = infos_list.find_element(By.CSS_SELECTOR,"a").get_attribute("href")
+    print("Company URL: " + company_url)
+    time.sleep(3)
+
+    job_des = driver.find_element(By.ID,"job-details").text
+    print("Job Des: " + job_des)
+    time.sleep(3)
+
+    #company screen
+    driver.execute_script("window.open('');")
+    company_window = driver.window_handles[2]
+    driver.switch_to.window(company_window)
+    company_about_url = company_url.replace("/life", "/about")
+    driver.get(company_about_url)
+    time.sleep(3)
+        
+    text_bodys = driver.find_elements(By.CLASS_NAME,"text-body-medium")
+    index = 0
+    for text in text_bodys:
+        time.sleep(2)
+        index += 1
+        print(str(index) + ": " +  text.text)
+        
+    time.sleep(5)
+    
+    driver.execute_script("window.open('');")
+    jobs_company_window = driver.window_handles[3]
+    driver.switch_to.window(jobs_company_window)
+    jobs_company_url = company_url.replace("/life", "/jobs")
+    driver.get(jobs_company_url)
+    time.sleep(3)
+    show_all_button = driver.find_element(By.CLASS_NAME,"link-without-hover-visited")
+    show_all_button.click()
+    time.sleep(3)
+    
+    #get list jobs
+    jobs = driver.find_elements(By.CLASS_NAME,"job-card-container__link")
+    print("Please Zoom in then press Enter")
+    count = len(jobs)
+    #input()
+    print(count)
+    
+    for job in jobs:
+        time.sleep(3)
+        job_title = job.text
+        print(job_title)
+        
+    driver.close()#close  jobs_company_window
+    time.sleep(2)
+    driver.switch_to.window(company_window)
+    time.sleep(2)
+    driver.close()#close  company_window
+    driver.switch_to.window(job_detail_window)
+    time.sleep(2)
+    driver.close()#close  job_detail_window
+    time.sleep(3)
+    driver.switch_to.window(root_window)  
 
 def get_lk_credentials(path="./lk_credentials.json"):
     f = open(path)
---
+++ linkedin_search.py
@@ -0,0 +1,287 @@
+"""
+This is meant to be run as a CLI script.
+It will scrap the search results of a LinkedIn Sales Navigator search.
+
+Example usage:
+python lksn_search_scraper.py --search-url "https://www.linkedin.com/sales/search/people?query=(spellCorrectionEnabled%3Atrue%2Ckeywords%3Ascraping)"
+"""
+import argparse
+import os
+import logging
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import pandas as pd
+from general_lk_utils import (
+    get_lk_credentials,
+    enter_ids_on_lk_signin,
+    get_job_detail
+)
+
+SCROLL_TO_BOTTOM_COMMAND = (
+    "document.getElementById('search-results-container').scrollTop+=100000;"
+)
+LK_CREDENTIALS_PATH = "./lk_credentials.json"
+
+
+def get_name_info_from_result_el(result_el):
+    selector = "div > div > div.flex.justify-space-between.full-width > div.flex.flex-column > div.mb3 > div > div.artdeco-entity-lockup__content.ember-view > div.flex.flex-wrap.align-items-center > div.artdeco-entity-lockup__title.ember-view > a"
+    els = result_el.select(selector)
+    link_to_profile = ""
+    name = ""
+    if len(els) > 0:
+        link_to_profile = els[0]["href"]
+        el_contents = els[0].contents
+        if len(el_contents) > 0:
+            name = el_contents[0].strip()
+    return {"name": name, "link_to_profile": link_to_profile}
+
+
+def get_connection_level_info_from_result_el(result_el):
+    selector = "div > div > div.flex.justify-space-between.full-width > div.flex.flex-column > div.mb3 > div > div.artdeco-entity-lockup__content.ember-view > div.flex.flex-wrap.align-items-center > div.artdeco-entity-lockup__badge.ember-view.ml1 > span.artdeco-entity-lockup__degree"
+    els = result_el.select(selector)
+    connection_level = ""
+    if len(els) > 0:
+        link_to_profile = els[0]
+        el_contents = els[0].contents
+        if len(el_contents) > 0:
+            connection_level = el_contents[0].strip().replace("·\xa0", "")
+    return {"connection_level": connection_level}
+
+
+def get_search_url(search_url_base, page=1):
+    url = search_url_base + f"&page={page}"
+    return url
+
+
+def get_linkedin_premium_info_from_result_el(result_el):
+    selector = "div > div > div.flex.justify-space-between.full-width > div.flex.flex-column > div.mb3 > div > div.artdeco-entity-lockup__content.ember-view > div.inline-flex > div > li-icon"
+    els = result_el.select(selector)
+    has_linkedin_premium = False
+    if len(els) > 0:
+        has_linkedin_premium = True
+    return {"has_linkedin_premium": has_linkedin_premium}
+
+
+def get_role_info_from_result_el(result_el):
+    selector = "div > div > div.flex.justify-space-between.full-width > div.flex.flex-column > div.mb3 > div > div.artdeco-entity-lockup__content.ember-view > div.artdeco-entity-lockup__subtitle.ember-view.t-14 > span"
+    els = result_el.select(selector)
+    role_name = ""
+    if len(els) > 0:
+        el_contents = els[0].contents
+
+        if len(el_contents) > 0:
+            role_name = el_contents[0].strip()
+    return {"role_name": role_name}
+
+
+def get_company_info_from_result_el(result_el):
+    selector = "div > div > div.flex.justify-space-between.full-width > div.flex.flex-column > div.mb3 > div > div.artdeco-entity-lockup__content.ember-view > div.artdeco-entity-lockup__subtitle.ember-view.t-14 > a"
+    els = result_el.select(selector)
+    link_to_company = ""
+    company_name = ""
+    if len(els) > 0:
+        link_to_company = els[0]["href"]
+        el_contents = els[0].contents
+        if len(el_contents) > 0:
+            company_name = el_contents[0].strip()
+    return {"link_to_company": link_to_company, "company_name": company_name}
+
+
+def get_time_in_company_info_from_result_el(result_el):
+    selector = "div > div > div.flex.justify-space-between.full-width > div.flex.flex-column > div.mb3 > div > div.artdeco-entity-lockup__content.ember-view > div.artdeco-entity-lockup__metadata.ember-view"
+    els = result_el.select(selector)
+    time_in_company = ""
+    if len(els) > 0:
+        el_contents = els[0].contents
+        if len(el_contents) > 0:
+            time_in_company = " | ".join(
+                [
+                    el_contents[i].strip().replace("\xa0", " ")
+                    for i in range(len(el_contents))
+                    if "<span" not in str(el_contents[i])
+                ]
+            )
+    return {"time_in_company": time_in_company}
+
+
+def get_additional_info_from_result_el(result_el):
+    selector = "div > div > div.flex.justify-space-between.full-width > div.flex.flex-column > div.ml8.pl1 > dl > div > dd > div > span"
+    els = result_el.select(selector)
+    additional_info = ""
+    if len(els) > 1:
+        el_contents = els[1].contents
+        if len(el_contents) > 0:
+            additional_info = " | ".join(
+                [
+                    el_contents[i].strip().replace("\xa0", " ")
+                    for i in range(len(el_contents))
+                    if "<span" not in str(el_contents[i])
+                    and "<button" not in str(el_contents[i])
+                ]
+            )
+    return {"additional_info": additional_info}
+
+
+def get_info_from_result_el(result_el):
+    r = []
+    r.append(get_name_info_from_result_el(result_el))
+    r.append(get_connection_level_info_from_result_el(result_el))
+    r.append(get_linkedin_premium_info_from_result_el(result_el))
+    r.append(get_role_info_from_result_el(result_el))
+    r.append(get_company_info_from_result_el(result_el))
+    r.append(get_time_in_company_info_from_result_el(result_el))
+    r.append(get_additional_info_from_result_el(result_el))
+
+    info = {}
+
+    for obj in r:
+        for k in obj.keys():
+            info[k] = obj[k]
+
+    info["time_scraped"] = int(time.time() * 1000)
+    return info
+
+
+def get_result_els(page_source):
+    soup = BeautifulSoup(page_source, "html.parser")
+    full_results_selector = "#search-results-container > div > ol > li"
+    all_results_el = soup.select(full_results_selector)
+    return all_results_el
+
+
+def get_all_info_from_page_source(page_source):
+    print("Getting all result elements...")
+    result_els = get_result_els(page_source)
+    n = len(result_els)
+    print(f"Found {n} elements.")
+
+    print("Getting the info for all elements...")
+    infos = []
+    for i in tqdm(range(n)):
+        new_info = get_info_from_result_el(result_els[i])
+        infos.append(new_info)
+    return infos
+
+
+def get_all_info_from_search_url(
+    driver, url, wait_after_page_loaded=3, wait_after_scroll_down=2
+):
+    driver.get(url)
+    print(f"Waiting for {wait_after_page_loaded}s...")
+    time.sleep(wait_after_page_loaded)
+
+    # Chrome must be unzoomed so that the whole page fits in the screen in 2 times
+    try:
+        driver.execute_script(SCROLL_TO_BOTTOM_COMMAND)
+    except:
+        print("There was an error scrolling down")
+    print(f"Waiting for {wait_after_scroll_down}s...")
+    time.sleep(wait_after_scroll_down)
+    page_source = driver.page_source
+    page_parsed_info = get_all_info_from_page_source(page_source)
+    return page_parsed_info
+
+
+def scrap_lksn_pages(
+    driver,
+    page_list,
+    get_search_url,
+    wait_time_between_pages=3,
+    wait_after_page_loaded=3,
+    wait_after_scroll_down=2,
+):
+    total_info = []
+    for p in page_list:
+        print(f"Waiting for {wait_time_between_pages}s...")
+        time.sleep(wait_time_between_pages)
+
+        print(f"Getting new page: {p}.")
+        info = get_all_info_from_search_url(
+            driver,
+            get_search_url(p),
+            wait_after_page_loaded=wait_after_page_loaded,
+            wait_after_scroll_down=wait_after_scroll_down,
+        )
+        total_info += info
+    return total_info
+
+
+if __name__ == "__main__":
+    
+    home_url = "https://www.linkedin.com/jobs/search"
+
+    print("Starting the driver...")
+    logging.getLogger("selenium").setLevel(logging.CRITICAL)
+    # Start the webdriver without any logs
+    driver = webdriver.Chrome(options=Options())
+    driver.maximize_window()
+    driver.get("https://www.linkedin.com/login/")
+
+    print("Inputting the credentials...")
+    lk_credentials = get_lk_credentials(LK_CREDENTIALS_PATH)
+    enter_ids_on_lk_signin(driver, lk_credentials["email"], lk_credentials["password"])
+
+    if "checkpoint/challenge" in driver.current_url:
+        print(
+            "It looks like you need to complete a double factor authentification. Please do so and press enter when you are done."
+        )
+        input()
+
+    driver.get(home_url)
+
+    print("Starting the scraping...")
+
+    #await driver.wait(until.elementLocated(By.className('link')), 1000);
+
+    #titleInputElement = driver.find_element(By.ID, "jobs-search-box-keyword-id-ember100")
+    titleInputElement = driver.find_element(By.CSS_SELECTOR,'[id*="jobs-search-box-keyword-id"]')
+    titleInputElement.clear()
+    titleInputElement.send_keys("ios developer")
+    
+    locationInputElement = driver.find_element(By.CSS_SELECTOR, '[id*="jobs-search-box-location-id"]')
+    locationInputElement.clear()
+    locationInputElement.send_keys("Malaysia")
+
+    #jobs-search-box__submit-button
+    
+    searchButton = driver.find_element(By.CLASS_NAME,"jobs-search-box__submit-button")
+    searchButton.click()
+    time.sleep(10)
+    
+    #get list jobs
+    jobs = driver.find_elements(By.CLASS_NAME,"job-card-container")
+    # jobs = driver.find_elements(By.CSS_SELECTOR,"#main > div > div:nth-child(1) > div > ul > li > div")
+    print("Please Zoom in then press Enter")
+    count = len(jobs)
+    #input()
+    print(count)
+    
+    for job in jobs:
+        #window_before = driver.window_handles[0]
+        #print(job.get_dom_attribute)
+        #job.click()
+        time.sleep(3)
+        #job-details-jobs-unified-top-card__job-title
+        #job_detail = driver.find_element(By.CLASS_NAME,"job-details-jobs-unified-top-card__job-title")
+        # job_detail = driver.find_element(By.CSS_SELECTOR,"#main > div> div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)")
+        # print(job_detail.text)
+        # job_detail.click()
+        #job_detail.click()
+        #window_after = driver.window_handles[-1]
+        #driver.switch_to.window(window_after)
+        
+        #jobs = driver.find_elements(By.CLASS_NAME,"#main > div > div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h2 > a")
+        # time.sleep(3)
+        # print(job)
+        # time.sleep(3)
+        job_id = job.get_attribute("data-job-id")
+        print(job_id)
+        get_job_detail(driver,job_id)
+
+    #driver.close()
